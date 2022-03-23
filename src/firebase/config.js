9a2725f5eb5b94dae10de3719bec16de22b50046
keerthana <keerthanamanoj999@gmail.com>
2022-03-23 15:25:19 +0530

    mk

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAGKLaBvYTEKv8nyySPV_H1yTFBIm5IFXc",
+    authDomain: "olxdemo-4eed6.firebaseapp.com",
+    projectId: "olxdemo-4eed6",
+    storageBucket: "olxdemo-4eed6.appspot.com",
+    messagingSenderId: "808075422337",
+    appId: "1:808075422337:web:5ce213c0b26f6368c8bb80",
+    measurementId: "G-5KYZLG3T17"
+  };
+
+
+  export const firebase = firebase.initializeApp(firebaseConfig)
